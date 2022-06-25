b8830190a4d6aa12c71beb9c7056a8902161e3f9
TEST2-jpg <agapdag@gmail.com>
2022-06-25 09:25:32 +0800

    Added nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,50 @@
+import { ReactComponent as User } from '../assets/User.svg'
+import { ReactComponent as Notif } from '../assets/Notif.svg'
+import { ReactComponent as Chat } from '../assets/Chat.svg'
+
+
+
+const Nav = (props) => {
+    const { status, signIn } = props
+    return (
+        <div className="nav">
+            <div className="lheader">
+                <img className="logo" src="https://scontent.fmnl9-1.fna.fbcdn.net/v/t39.30808-6/275109346_1577887262572313_2738524034509449148_n.png?_nc_cat=109&ccb=1-7&_nc_sid=9267fe&_nc_ohc=SX3wgBFpjSIAX9rlihz&_nc_ht=scontent.fmnl9-1.fna&oh=00_AT_8M_LSpSui4dVGuKhS07hpEig_ESQyrRymn2Wq3tnujQ&oe=62B9BE9A" />
+                <div className="nav--links">
+                    <div className="nav--content">Find jobs</div>
+                    <div className="nav--content">Company reviews</div>
+                    <div className="nav--content">Find salaries</div>
+                </div>
+            </div>
+            <div className="lheader">
+                {!status ?
+                    <div className="nav--links">
+                        <div className="nav--content">Create your profile</div>
+                        <div className="nav--content signin" onClick={signIn}>Sign in</div>
+                        <div className='line'></div>
+
+                        <div className="postjob">Employers / Post job</div>
+                    </div>
+                    :
+                    <div className="nav--links">
+                        <div className="nav--content nav--logo">
+                            <Chat className='chatLogo' />
+                        </div>
+                        <div className="nav--content nav--logo">
+                            <Notif className='bellLogo' />
+                        </div>
+                        <div className="nav--content nav--logo">
+                            <User className='userLogo' />
+                        </div>
+                        <div className='line'></div>
+                        <div className="postjob">Employers / Post job</div>
+
+                    </div>
+                }
+            </div>
+
+        </div>
+    )
+}
+
+export default Nav
