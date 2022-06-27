ae1a8f7c83f0524c9e0c66f6600bebb0c46bbb80
TEST2-jpg <agapdag@gmail.com>
2022-06-27 16:03:31 +0800

    Added job cards

---
+++ src/components/Jobdetails.js
@@ -0,0 +1,19 @@
+import { ReactComponent as Money } from '../assets/Money.svg'
+
+const Jobdetails = ({ title, company, location, type, pay, description, time, shift }) => {
+    return (
+        <div className='card'>
+            <div className="title">{title}</div>
+            <div className="company">{company}</div>
+            <div className="location">{location}</div>
+            <div className="container">
+                {pay && <div className="pay adds"><Money className='dsvglogo'/>{pay}</div>}
+                {type && type.map( x => <div className="type adds">{x}</div>)}
+                {shift && shift.map( x => <div className="type adds">{x}</div>)}
+            </div>
+            <div className="description">{description.slice(0,270)}...</div>
+        </div>
+    )
+}
+
+export default Jobdetails
