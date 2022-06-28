6f1e6f29d1a3325b1d65cb04767fd7c943ee8142
TEST2-jpg <agapdag@gmail.com>
2022-06-28 15:41:10 +0800

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Jobfeed from './components/Jobfeed';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Jobfeed/>      
+    </div>
+  );
+}
+
+export default App;
