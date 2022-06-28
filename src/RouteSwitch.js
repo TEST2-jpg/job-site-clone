16413b149b1b58c5ce95fcf9e55e3c6d8f634597
TEST2-jpg <agapdag@gmail.com>
2022-06-28 15:42:04 +0800

    React routes file

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,14 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Jobform from "./components/Jobform";
 import React from "react";
 import App from "./App";
-import Details from "./components/forms/Details";
+import Card from "./components/Card";
 
 const RouteSwitch = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<App />} />
+                <Route path="/" element={<App />}>
+                    <Route path="q/:id" element={<Card />} />
+                </Route>
                 <Route path="/forms" element={<Jobform />} />
+                <Route path="*" element={<App />} />
             </Routes>
         </BrowserRouter>
     );
