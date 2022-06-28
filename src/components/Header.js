a622f950ed32f6cc55702a33523ddc6bebae4705
TEST2-jpg <agapdag@gmail.com>
2022-06-28 15:43:43 +0800

    removed unused vars

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,35 +10,16 @@ const Header = () => {
     async function signIn() {
         const provider = new GoogleAuthProvider();
         await signInWithPopup(auth, provider);
-        console.log('13121313', auth.currentUser === getAuth())
     }
     function signOutUser() {
-        console.log('outed', auth.currentUser)
         signOut(auth);
     }
     function isUserSignedIn() {
-        console.log(getAuth().currentUser)
         return !!getAuth().currentUser;
-    }
-
-    function getProfilePicUrl() {
-        return auth.currentUser.photoURL || '/images/profile_placeholder.png';
-    }
-
-    function getUserName() {
-        return auth.currentUser.displayName;
     }
     return (
         <>  
             {<Nav status={isUserSignedIn()} signIn={signIn} signOut={signOutUser} />}
-            <button onClick={signIn}>Sign In</button>
-            <button onClick={signOutUser}>Sign out</button>
-            <button onClick={() => {
-                console.log(user)
-                console.log(isUserSignedIn())
-                console.log(getProfilePicUrl())
-                console.log(getUserName())
-            }}>user out</button>
         </>
     )
 }
